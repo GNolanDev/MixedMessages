7b4560e76eeaba1fe8519b99ab2734451efdca7f
gingergaz <gaz@cliffhanger.com>
2021-06-16 11:01:49 +0100

    Add outline plan for main.js in comments

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // script to add random football related comments to the text output of index.html
-// test of runnability in node
-console.log("Script is working!");
+// test of runnability in node. NB: type 'node main.js' in bash
+// console.log("Script is working!");
+
+// get button and text output elements, & add event listener to enable button click function
+
+// import json of all teams in competition, and separate quotes of commentators and managers - store in arrays
+
+// create first part of string: one team vs another, ensuring a team doesn't play itself
+
+// create remaining string by concatenating one random selection from each of the two quote stores
+
+// output text to text box
