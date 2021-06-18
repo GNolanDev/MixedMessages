926904af2b7023f783e7c33d4a6deca5cde18f88
gingergaz <gaz@cliffhanger.com>
2021-06-18 14:08:31 +0100

    Add button event listener to make fully functional

--- main.js
+++ main.js
@@ -12,14 +12,23 @@ const getNewTeams = (tList) => {
   return [t1, t2];
 };
 
-// given an array of team names & an html element to output to, set innerHTML as the message string
-const showNewMessage = (tList, outputElement) => {
+// given arrays of team names and quotes & an html element to output to, set innerHTML as the message string
+const showNewMessage = (tList, cList, aList, outputElement) => {
   let outputString = "";
   let [team1, team2] = getNewTeams(tList);
+  let commentatorMessage = getRandomMessage(cList);
+  let afterMessage = getRandomMessage(aList);
   //console.log(team1, team2);
-  outputString = `In the fantastic game of ${team1} vs ${team2}, the best quotes of the day were...<br>`;
+  outputString =
+    `In the fantastic game of ${team1} vs ${team2}, the best quotes of the day were...<br>` +
+    `From the in-game commentary: <blockquote>"${commentatorMessage}"</blockquote><br>` +
+    `From after match interviews: <blockquote>"${afterMessage}"</blockquote>`;
   outputElement.innerHTML = outputString;
 };
+
+// given array of strings, select a random string & return it
+const getRandomMessage = (msgArray) =>
+  msgArray[Math.floor(Math.random() * msgArray.length)];
 
 // get button and text output elements, & add event listener to enable button click function
 let startButton, outputDiv;
@@ -27,14 +36,11 @@ window.onload = (event) => {
   // assignment happens here to ensure page has loaded before attempting to get elements
   startButton = document.getElementById("change-button");
   outputDiv = document.getElementById("output-text");
-  // test this part:
+  //tests:
   //outputDiv.innerHTML = "output text test";
   //outputDiv.innerHTML = String(teamsList);
-  showNewMessage(teamsList, outputDiv);
+  //showNewMessage(teamsList, commentatorQuotes, afterQuotes, outputDiv);
+  startButton.addEventListener("click", (ev) => {
+    showNewMessage(teamsList, commentatorQuotes, afterQuotes, outputDiv);
+  });
 };
-
-
-
-// create remaining string by concatenating one random selection from each of the two quote stores
-
-// output text to text box
